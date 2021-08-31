985ccc199f8b5b863b5dbdada4d587de2e0b6d55
Long Thiên Đinh <you@example.com>
2021-08-31 17:11:28 +0700

    andsjadn

---
+++ src/com/dinhlongthien/Main.java
@@ -0,0 +1,11 @@
+package com.dinhlongthien;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> movie = new ArrayList<>();
+    }
+}
